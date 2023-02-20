5f2a97636b5d667922f04cc217c1b650c83366e3
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-21 00:27:25 +0530

    refactor: write styles for Backlog using styled-components, delete Backlog.css file

--- src/pages/Backlog/Backlog.js
+++ src/pages/Backlog/Backlog.js
@@ -1,6 +1,27 @@
+import styled from "styled-components";
 import Task from "../../components/Task/Task";
 import useDataFetching from "../../hooks/useDataFetching"
-import "./Backlog.css"
+
+const BacklogWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin: 5%;
+`;
+
+const Title = styled.h2`
+	width: 100%;
+	padding-bottom: 10px;
+	text-align: left;
+	border-bottom: 1px solid darkgray;
+`;
+
+const TasksWrapper = styled.div`
+	display: flex;
+	justify-content: space-between;
+	flex-direction: row;
+	flex-wrap: wrap;
+	margin: 5%;
+`;
 
 function Backlog() {
 	const [loading, error, tasks] = useDataFetching(
@@ -8,9 +29,9 @@ function Backlog() {
 	);
 
 	return (
-		<div className="Backlog-wrapper">
-			<h2>Backlog</h2>
-			<div className="Tasks-wrapper">
+		<BacklogWrapper>
+			<Title>Backlog</Title>
+			<TasksWrapper>
 				{loading || error ? (
 					<span>{error || "Loading..."}</span>
 				) : (
@@ -22,8 +43,8 @@ function Backlog() {
 						/>
 					))
 				)}
-			</div>
-		</div>
+			</TasksWrapper>
+		</BacklogWrapper>
 	)
 }
 
