49279355015488b723788ce1824486e5f0c00134
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-20 17:01:55 +0530

    feat: write Backlog component to render backlog tasks, use react-router for routing between home and backlog pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Board from './pages/Board/Board';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Backlog from "./pages/Backlog/Backlog";
+import Board from "./pages/Board/Board";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Board />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Board />} />
+          <Route path="/backlog" element={<Backlog />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
