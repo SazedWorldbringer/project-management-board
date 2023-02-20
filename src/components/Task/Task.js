97a9feb4f0e8512a2da3046defa735cf10244113
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-20 23:57:54 +0530

    feat: install styled-components and update Task component to use it

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,15 +1,30 @@
-import "./Task.css";
+import styled from "styled-components";
+
+const TaskWrapper = styled.div`
+	background: darkgray;
+	padding: 20px;
+	border-radius: 20px;
+	margin: 0% 5% 5% 5%;
+	h3 {
+		width: 100%;
+		margin: 0;
+	}
+`;
+
+const Title = styled.h3`
+	width: 100%;
+	margin: 0;
+`;
 
 function Task({ id, title, body, onDragStart }) {
 	return (
-		<div
-			className="Task-wrapper"
+		<TaskWrapper
 			draggable
 			onDragStart={onDragStart}
 		>
-			<h3>{title}</h3>
+			<Title>{title}</Title>
 			<p>{body}</p>
-		</div>
+		</TaskWrapper>
 	)
 }
 
