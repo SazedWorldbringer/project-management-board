0b8151a0e8d4c23b247128e24b520bc3b885ceb0
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-20 23:21:18 +0530

    fix: add onDragOver prop to prevent the default behaviour

--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -1,9 +1,12 @@
 import Task from '../Task/Task';
 import './Lane.css'
 
-function Lane({ title, loading, error, tasks, onDragStart }) {
+function Lane({ title, loading, error, tasks, onDragStart, onDragOver }) {
 	return (
-		<div className="Lane-wrapper">
+		<div
+			className="Lane-wrapper"
+			onDragOver={onDragOver}
+		>
 			<h2>{title}</h2>
 			{loading || error ? (
 				<span>{error || 'Loading...'}</span>
