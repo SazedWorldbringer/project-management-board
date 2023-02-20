2e1f5242fa7294de5e4d28d3ade46d72de1b6138
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-21 00:12:31 +0530

    refactor: write global styles for the app using styled components, delete import for index.css from the index.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import Header from "./components/Header/Header";
 import Backlog from "./pages/Backlog/Backlog";
 import Board from "./pages/Board/Board";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 const AppWrapper = styled.div`
   text-align: center;
 `;
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
 function App() {
   return (
-    <AppWrapper>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Board />} />
-          <Route path="/backlog" element={<Backlog />} />
-        </Routes>
-      </BrowserRouter>
-    </AppWrapper>
+    <>
+      <GlobalStyle />
+      <AppWrapper>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Board />} />
+            <Route path="/backlog" element={<Backlog />} />
+          </Routes>
+        </BrowserRouter>
+      </AppWrapper>
+    </>
   );
 }
 
